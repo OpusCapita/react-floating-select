249f919b71bbed51e01e8f38cd1ea2c809cc3456
Sami Karjalainen <sami.karjalainen@opuscapita.com>
2017-09-05 11:49:07 +0300

    Extracted component from oc-common-ui

---
+++ webpack/base.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+function getBaseConfiguration(config) {
+  return {
+    entry: config.entry,
+    devtool: 'source-map',
+    output: config.output,
+    module: {
+      rules: [
+        {
+          test: /(\.jsx|\.js)$/,
+          use: [
+            'babel-loader',
+          ],
+          exclude: /(node_modules|bower_components)/,
+        },
+        {
+          test: /\.svg$/,
+          use: ['babel-loader', 'react-svg-loader'],
+        },
+      ],
+    },
+    resolve: {
+      modules: [
+        path.resolve('./src'),
+        'node_modules',
+      ],
+      extensions: ['.js', '.jsx'],
+    },
+    plugins: [
+      new CleanWebpackPlugin([config.buildPath], {
+        root: config.root,
+        verbose: false,
+        dry: false,
+      }),
+      new webpack.optimize.ModuleConcatenationPlugin(),
+    ],
+  };
+}
+
+module.exports = getBaseConfiguration;
